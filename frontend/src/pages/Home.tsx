ca1b7ca4bc321f23b2fa35baad272ccb3b1d8a85
b-l-u-e <winnie.gitau282@gmail.com>
2024-09-08 19:36:06 +0300

    updated the home page to include credit score, loan repayments

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const Home: React.FC = () => {
     return (
-      
-          
-            <Container>
-                <Box my={4}>
-                    <Typography variant="h4" component="h1" gutterBottom>
-                        Welcome to the Decentralized Credit Platform
-                    </Typography>
+        <Container>
+            <Box my={4}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Welcome to the Decentralized Credit Platform
+                </Typography>
+                <Box mt={2}>
+                    <Button variant="contained" color="primary" component={Link} to="/credit-score">
+                        Credit Score
+                    </Button>
                 </Box>
-            </Container>
-        
-    
+                <Box mt={2}>
+                    <Button variant="contained" color="primary" component={Link} to="/loan-repayments">
+                        Loan Repayments
+                    </Button>
+                </Box>
+                <Box mt={2}>
+                    <Button variant="contained" color="primary" component={Link} to="/lending-pools">
+                        Lending Pools
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
