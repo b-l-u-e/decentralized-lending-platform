a474a764ea061c18759019b113fa11f9e75ead2a
b-l-u-e <winnie.gitau282@gmail.com>
2024-09-08 20:29:15 +0300

    updated the pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import CreditScore from './pages/CreditScore';
-import LendingPools from './pages/LendingPools';
-import LoanRepayments from './pages/LoanRepayments';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CreditScore from "./pages/user/CreditScore";
+import LendingPools from "./pages/user/LendingPools";
+import LoanRepayments from "./pages/user/LoanRepayments";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/credit-score" element={<CreditScore />} />
-                <Route path="/lending-pools" element={<LendingPools />} />
-                <Route path="/loan-repayments" element={<LoanRepayments />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/credit-score" element={<CreditScore />} />
+        <Route path="/lending-pools" element={<LendingPools />} />
+        <Route path="/loan-repayments" element={<LoanRepayments />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
